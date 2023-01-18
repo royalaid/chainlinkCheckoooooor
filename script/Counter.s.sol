3e67fc89bc997595044779b13c360b40dd4af27a
Mark Aiken <2439803+royalaid@users.noreply.github.com>
2023-01-17 23:09:22 -0800

    POC Checker

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,25 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import "./interfaces/IChainlinkFeed.sol";
 
 contract CounterScript is Script {
-    function setUp() public {}
+    IChainlinkFeed feed;
+
+    function setUp() public {
+        feed = IChainlinkFeed(0xDE31F8bFBD8c84b5360CFACCa3539B938dd78ae6);
+    }
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        (,int256 answer,,,) = feed.latestRoundData();
+        console.log("Latest Round:");
+        console.logInt(answer);
+        console.log("Latest Answer:");
+        console.logInt(feed.latestAnswer());
+        console.log("Current Blocktimestamp:");
+        console.log(block.timestamp);
+        vm.stopBroadcast();
     }
 }
